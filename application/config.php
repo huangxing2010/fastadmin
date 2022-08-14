5a54dbfda471e974045745b310c3231edc12e7dc
huangxing <1398474457@qq.com>
2022-08-14 16:08:30 +0800

    V1.3.4.20220530 No Install
    Signed-off-by: huangxing <1398474457@qq.com>

--- application/config.php
+++ application/config.php
@@ -253,7 +253,7 @@ return [
         // 驱动方式
         'type'     => 'Mysql',
         // 缓存前缀
-        'key'      => 'V9dYISbspEMqtnm8QLJRZPlozNBk2egT',
+        'key'      => 'i3d6o32wo8fvs1fvdpwens',
         // 加密方式
         'hashalgo' => 'ripemd160',
         // 缓存有效期 0表示永久缓存
